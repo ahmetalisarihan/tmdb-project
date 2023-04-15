ff33e5c1c469404bf03f7390066bf7e44344df6e
ahmetalisarihan <ahmetalisarihan@gmail.com>
2023-04-16 00:19:27 +0300

    Add Menu

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,7 +1,10 @@
-export const Container = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import { CustomComponentProps } from "../interfaces";
+import { mergeClassName } from "../utils";
+
+export const Container = (props: CustomComponentProps) => {
+  return (
+    <div className={mergeClassName("px-6 py-3 max-w-screen-lg mx-auto",)}>
+      {props.children}
+    </div>
+  );
+};
--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "../components/container";
+import { Link, useLocation } from "react-router-dom";
+import { mergeClassName } from "../utils";
+
+const MENU_CLASS = `
+p-1.5
+hover:bg-primary
+rounded-md
+`
+
+const MENU_CLASS_ACTIVE = `bg-primary`
 
 export const Header = () => {
+    const location = useLocation()
+    const [pathname, setPathname] = useState('')
+    const getMenuClass = (path: string)=>{
+        if(path === pathname){
+            return mergeClassName(MENU_CLASS, MENU_CLASS_ACTIVE)
+        }
+        return mergeClassName(MENU_CLASS, '')
+    }
+
+    useEffect(()=> {
+        setPathname(location.pathname)
+    }, [location.pathname])
+
   return (
-    <div>
-      <Container></Container>
+    <div className="bg-header ">
+      <Container clasName="flex item-center justify-between">
+        <div className="flex items-center gap-6">
+            <h1 className="text-2xl font-semibold">
+                <Link to={'./'}>TMDB</Link>
+            </h1>
+            <div className="
+            pt-1.5
+            flex 
+            items-center 
+            gap-1.5">
+                <Link className={getMenuClass('./movies')} to={'./movies'}>Movies</Link>
+                <Link className={getMenuClass('./tv')} to={'./tv'}>TV</Link>
+            </div>
+        </div>
+      </Container>
     </div>
   );
 };
